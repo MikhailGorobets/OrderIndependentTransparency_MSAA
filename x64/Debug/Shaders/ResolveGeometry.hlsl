04ab891c0df6aefab753db549a026cceb6bf1547
Mikhail Gorobets <misha.friman@mail.ru>
2021-01-30 01:18:21 +0500

    Update ResolveGeometry.hlsl

--- x64/Debug/Shaders/ResolveGeometry.hlsl
+++ x64/Debug/Shaders/ResolveGeometry.hlsl
@@ -8,14 +8,12 @@ RWTexture2D<unorm float4>  BackBuffer      : register(u0);
 Texture2D<uint>            HeadPointersSRV : register(t0);
 StructuredBuffer<ListNode> LinkedListSRV   : register(t1);
 
-
 [numthreads(8, 8, 1)]
 void CSMain(uint3 id: SV_DispatchThreadID) {
        
     float4 backBuffer    = BackBuffer[id.xy];
     float4 resolveBuffer = float4(0.0, 0.0, 0.0, 0.0f);
     
-
     uint nodeHead = HeadPointersSRV[id.xy];
     if (nodeHead == 0xFFFFFFFF)
         return;
@@ -52,7 +50,6 @@ void CSMain(uint3 id: SV_DispatchThreadID) {
             dstPixelColor = lerp(dstPixelColor, srcPixelColor, srcPixelColor.a);
         }
         resolveBuffer += dstPixelColor;
-    }
-   
+    }  
     BackBuffer[id.xy] = resolveBuffer / MSAA_SAMPLE_COUNT;
 }
